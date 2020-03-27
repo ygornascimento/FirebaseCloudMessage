f797bc03fab48df6ee6fff6ceac5066a8daa3978
Ygor Nascimento <nascimento.ygor@outlook.com>
2020-03-27 10:32:41 -0300

    Added Xcode Notification Extension

---
+++ FCM/NotificationServicesExtension/NotificationService.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationService.swift
+//  NotificationServicesExtension
+//
+//  Created by Ygor Nascimento on 27/03/20.
+//  Copyright © 2020 Ygor Nascimento. All rights reserved.
+//
+
+import UserNotifications
+import FirebaseMessaging
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            bestAttemptContent.title = "\(bestAttemptContent.title)"
+            
+            //contentHandler(bestAttemptContent)
+            Messaging.serviceExtension().populateNotificationContent(bestAttemptContent, withContentHandler: contentHandler)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
